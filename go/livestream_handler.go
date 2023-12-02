f17b5b1357473f7b658e32bf13dc6e55c32bd48b
Daaaai0809 <dai.tsuruga@80and.co>
2023-12-03 01:50:57 +0900

    fix

--- go/livestream_handler.go
+++ go/livestream_handler.go
@@ -162,16 +162,18 @@ func reserveLivestreamHandler(c echo.Context) error {
 	}
 	livestreamModel.ID = livestreamID
 
-	livestreamTagModels := make([]*LivestreamTagModel, 0)
-	for _, tagID := range req.Tags {
-		livestreamTagModels = append(livestreamTagModels, &LivestreamTagModel{
-			LivestreamID: livestreamID,
-			TagID:        tagID,
-		})
-	}
+	if len(req.Tags) != 0 {
+		livestreamTagModels := make([]*LivestreamTagModel, 0)
+		for _, tagID := range req.Tags {
+			livestreamTagModels = append(livestreamTagModels, &LivestreamTagModel{
+				LivestreamID: livestreamID,
+				TagID:        tagID,
+			})
+		}
 
-	if _, err := tx.NamedExecContext(ctx, "INSERT INTO livestream_tags (livestream_id, tag_id) VALUES (:livestream_id, :tag_id)", livestreamTagModels); err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "failed to insert livestream tag: "+err.Error())
+		if _, err := tx.NamedExecContext(ctx, "INSERT INTO livestream_tags (livestream_id, tag_id) VALUES (:livestream_id, :tag_id)", livestreamTagModels); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "failed to insert livestream tag: "+err.Error())
+		}
 	}
 
 	livestream, err := fillLivestreamResponse(ctx, tx, *livestreamModel)
@@ -228,15 +230,6 @@ func searchLivestreamsHandler(c echo.Context) error {
 				return echo.NewHTTPError(http.StatusInternalServerError, "failed to get livestreams: "+err.Error())
 			}
 		}
-
-		// for _, keyTaggedLivestream := range keyTaggedLivestreams {
-		// 	ls := LivestreamModel{}
-		// 	if err := tx.GetContext(ctx, &ls, "SELECT * FROM livestreams WHERE id = ?", keyTaggedLivestream.LivestreamID); err != nil {
-		// 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get livestreams: "+err.Error())
-		// 	}
-
-		// 	livestreamModels = append(livestreamModels, &ls)
-		// }
 	} else {
 		// 検索条件なし
 		query := `SELECT * FROM livestreams ORDER BY id DESC`
